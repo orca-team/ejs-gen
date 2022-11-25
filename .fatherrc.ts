01c3edef8d9cc2eb9d94d972d722b34050519107
NicoKam <331677620@qq.com>
2022-11-25 12:08:22 +0800

    feat: with ts definition

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -2,5 +2,5 @@ import { defineConfig } from 'father';
 
 export default defineConfig({
   // more father config: https://github.com/umijs/father/blob/master/docs/config.md
-  cjs: { output: 'lib' },
+  cjs: { output: 'lib/' },
 });

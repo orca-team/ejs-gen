bec36eca1c333d9a6cb63f393a350caef8078072
NicoKam <331677620@qq.com>
2022-11-28 15:30:17 +0800

    feat: add suffix options

--- src/cli.ts
+++ src/cli.ts
@@ -22,11 +22,12 @@ program
   .argument('<target>', '目标目录')
   .description('将模板目录下的文件复制到目标目录，对所有的文件都会进行 ejs 模板替换')
   .option('--data <data>', '需要进行模板替换的内容(JSON格式)')
-  .option('--ejs-only [suffix]', '仅对后缀为 ejs 的文件进行模板内容替换')
+  .option('--ejs-only [ejs-suffix]', '仅对后缀为 ejs 的文件进行模板内容替换')
+  .option('--suffix [suffix]', '添加需要进行模板内容替换的文件后缀，如有多个使用英文逗号分隔')
   .option('--verbose')
   .action(async (source, target, options) => {
-    const { data = '{}' } = options;
-    await transformEjsDir(source, target, { ...options, data: JSON5.parse(data) });
+    const { data = '{}', suffix = '' } = options;
+    await transformEjsDir(source, target, { ...options, suffix: suffix.split(','), data: JSON5.parse(data) });
   });
 
 program.parseAsync().finally(async () => {});
--- src/index.ts
+++ src/index.ts
@@ -23,11 +23,14 @@ const cwd = process.cwd();
 
 export type TransformEjsDirOptions = {
   data?: Record<string, unknown>;
+  suffix?: string[];
   ejsOnly?: string | boolean;
 };
 
+const defaultSuffix = ['.txt', '.js', '.jsx', '.ts', '.tsx', '.json', '.ini', '.ejs'];
+
 export default async function transformEjsDir(source: string, target: string, options: TransformEjsDirOptions = {}) {
-  const { data = {}, ejsOnly } = options;
+  const { data = {}, suffix = [], ejsOnly } = options;
   const sourcePath = isAbsolute(source) ? source : join(cwd, source);
   logger.debug('sourcePath:', sourcePath);
   const fileList = await glob('**', {
@@ -68,45 +71,56 @@ export default async function transformEjsDir(source: string, target: string, op
           const targetFullPath = join(targetDir, targetPath);
           await fs.mkdir(targetFullPath, { recursive: true });
         } else if (stat.isFile()) {
+          // 判断后缀，是否支持的文件
+          let shouldTransform = true;
+
+          if (ejsOnly) {
+            let ejsSuffix = typeof ejsOnly === 'string' ? ejsOnly : '.ejs';
+            if (!ejsSuffix.startsWith('.')) ejsSuffix = `.${ejsSuffix}`;
+            shouldTransform = filePath.endsWith(ejsSuffix);
+            // ejsOnly mode 移除 ejs 后缀
+            if (targetPath.endsWith(ejsSuffix)) {
+              targetPath = targetPath.slice(0, targetPath.length - ejsSuffix.length);
+            }
+          } else {
+            shouldTransform = [...suffix, ...defaultSuffix].some((_suffix) => {
+              const s = _suffix.startsWith('.') ? _suffix : `.${_suffix}`;
+              return filePath.endsWith(s);
+            });
+          }
+
+          if (!shouldTransform) {
+            // 不需要进行字符串转换的文件，使用 copy 模式
+            const targetFullPath = join(targetDir, targetPath);
+            await fs.copyFile(fullPath, targetFullPath);
+            return;
+          }
+
           // read
           const file = await fs.readFile(fullPath);
           let fileContent = file.toString();
 
-          let shouldTransform = true;
-          if (ejsOnly) {
-            let ejsSuffix = typeof ejsOnly === 'string' ? ejsOnly : '.ejs';
-            if (!ejsSuffix.startsWith('.')) ejsSuffix = `.${ejsSuffix}`;
-            if (targetPath.endsWith(ejsSuffix)) {
-              targetPath = targetPath.slice(0, targetPath.length - ejsSuffix.length);
-            } else {
-              // do not transform
-              shouldTransform = false;
-            }
-          }
-
-          if (shouldTransform) {
-            fileContent = ejs.render(fileContent, {
-              ...data,
-              camelCase,
-              pascalCase,
-              paramCase,
-              noCase,
-              dotCase,
-              capitalCase,
-              pathCase,
-              snakeCase,
-              headerCase,
-              sentenceCase,
-              constantCase,
-            });
-          }
+          fileContent = ejs.render(fileContent, {
+            ...data,
+            camelCase,
+            pascalCase,
+            paramCase,
+            noCase,
+            dotCase,
+            capitalCase,
+            pathCase,
+            snakeCase,
+            headerCase,
+            sentenceCase,
+            constantCase,
+          });
 
           // write
           const targetFullPath = join(targetDir, targetPath);
           await fs.mkdir(dirname(targetFullPath), { recursive: true });
           await fs.writeFile(targetFullPath, fileContent);
         } else {
-          logger.warn(`Path [${fullPath}] ignored is not a file or directory.`)
+          logger.warn(`Path [${fullPath}] ignored is not a file or directory.`);
         }
       } catch (error: any) {
         logger.error('Error while transforming file:', fullPath);
